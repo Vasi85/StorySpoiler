71fc7f2bd0689ecdb82081776e9d63c16ff8a8ca
Vasi85 <vasi.mincheva@gmail.com>
2025-08-20 09:19:29 +0300

    new commit

--- StorySpoiler/obj/Release/net8.0/StorySpoiler.AssemblyInfo.cs
+++ StorySpoiler/obj/Release/net8.0/StorySpoiler.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("StorySpoiler")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e4637b95f2e4dc1c84a5d0ccd0b774adcca1b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StorySpoiler")]
